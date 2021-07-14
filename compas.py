c2738d9f07dc594f69830428aeccb38bda028b23
Tonix22 <emiliotonix@gmail.com>
2021-07-13 23:38:59 -0500

    update cordenate to more acurate data

--- compas.py
+++ compas.py
@@ -1,11 +1,21 @@
 import math
 
-winds = ["E", "SEE", "SE", "SSE", "S", "SSW", "SW", "SWW", "W", "NWW", "NW", "NNW","N", "NNE", "NE", "NEE"]
+#winds = ["E", "SEE", "SE", "SSE", "S", "SSW", "SW", "SWW", "W", "NWW", "NW", "NNW","N", "NNE", "NE", "NEE"]
+
+winds = ["E", 
+         "NEE", "NE", "NNE",
+         "N", 
+         "NNW","NW","NWW",
+         "W",
+         "SWW","SW","SSW",
+         "S",
+         "SSE","SE","SEE"]
+
 #360/16*n
 def winds_to_degree(sub_s):
-    for index, string in enumerate(winds):
-         if sub_s in string:
-            return index*22.5 #360/16
+    for i in range(0,len(winds)):
+        if(sub_s == winds[i]):
+            return i*2*math.pi/16 #360/16 but plot lib is in radians     
 
 def plot_speed_and_angle(radius,angle):
     x = radius*math.cos(angle)
